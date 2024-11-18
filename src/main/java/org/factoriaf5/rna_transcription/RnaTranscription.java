8e7a58e005b9f6f0de18ed43bd138528b82dec2c
Rubén Blanco <r.blanco.3d@gmail.com>
2024-11-18 23:43:58 +0100

    Method for RNA transcription implemented

--- src/main/java/org/factoriaf5/rna_transcription/RnaTranscription.java
+++ src/main/java/org/factoriaf5/rna_transcription/RnaTranscription.java
@@ -4,7 +4,30 @@ package org.factoriaf5.rna_transcription;
 public class RnaTranscription {
     
     String transcribe(String dnaStrand) {
-        throw new UnsupportedOperationException("Implement method");
+
+        String[] rnaTranscription = new String[dnaStrand.length()];
+
+        if (dnaStrand.isEmpty()) {
+            return "";
+        }
+        else {
+            for(int i=0; i<dnaStrand.length(); i++){
+                switch (dnaStrand.charAt(i)) {
+                    case 'C': rnaTranscription[i] = "G";
+                    break;
+                    case 'G': rnaTranscription[i] = "C";
+                    break;
+                    case 'T': rnaTranscription[i] = "A";
+                    break;
+                    case 'A': rnaTranscription[i] = "U";
+                    break;
+                }
+            }
+            String result = String.join("", rnaTranscription);
+            return result;
+        }
+
     }
 
 }
+
